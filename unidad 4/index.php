e086a3c4334ba50207f9c082d1dd7daaf14cf901
SegovianoJordy <71461787+Yord1412@users.noreply.github.com>
2022-09-15 18:43:23 -0600

    tarea 1 unidad 4
    inicio de less

---
+++ unidad 4/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		Panel
+	</title>
+	<link rel="stylesheet" type="text/css" href="public/CSS/main.css">
+	
+
+</head>
+<body>
+	<div class="container">
+		
+		<fieldset>
+			<form>
+				<legend>
+					acceso al panel
+				</legend>
+
+				<label>
+					correo
+				</label>
+
+				<input type="email" name="">
+
+				<label>
+					contraseña
+				</label>
+
+				<input type="password" name="">
+
+				<button type="submit">
+					acceder
+				</button>
+			</form>
+			
+		</fieldset>
+		
+	</div>
+	
+</body>
+
+</html>
