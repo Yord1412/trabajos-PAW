f6fd0cee468d8339e11acba110a1034b95adfb0d
SegovianoJordy <71461787+Yord1412@users.noreply.github.com>
2022-09-21 17:20:45 -0600

    actividad 5 login 4ta unidad
    creacion de login en bootstrap 5

--- unidad 4/index.php
+++ unidad 4/index.php
@@ -6,38 +6,78 @@
 	<title>
 		Panel
 	</title>
-	<link rel="stylesheet" type="text/css" href="public/CSS/main.css">
 	
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.recolor{
+			min-height: 600px;
+			
+		}
+	</style>
 
 </head>
 <body>
-	<div class="container">
+	<div class="container recolor">
+
+		<section>
+
+			<div class="row justify-content-md-center align-items-center">
+
+				<div class="col-md-6 col-sm-12">
+					<form>
+						<h1 class="text-center">
+							Acceso al panel
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							
+						</p>
+
+						<!--<input type="email" name="">-->
+
+
+						<div class="mb-3">
+							<label>
+								 Correo electronico
+							</label>
+							<div class="input-group mb-3">
+							  <span class="input-group-text" id="basic-addon1">@</span>
+							  <input type="text" class="form-control" placeholder="Correo electronico" aria-label="Username" aria-describedby="basic-addon1">
+							</div>
+						</div>
+
+
+						<div class="mb-3">
+							<label>
+								Contraseña
+							</label>
+
+							<!--<input type="password" name="">-->
+
+							<div class="input-group mb-3">
+							  <span class="input-group-text" id="basic-addon1">@</span>
+							  <input type="text" class="form-control" placeholder="******" aria-label="Username" aria-describedby="basic-addon1">
+							</div>
+						</div>
+						
+
+
+						<button class="btn btn-primary col-12" type="submit">
+							Acceder
+						</button>
+					</form>
+				</div>
+			</div>
+		</section>
 		
 		<fieldset>
-			<form>
-				<legend>
-					acceso al panel
-				</legend>
-
-				<label>
-					correo
-				</label>
-
-				<input type="email" name="">
-
-				<label>
-					contraseña
-				</label>
-
-				<input type="password" name="">
-
-				<button type="submit">
-					acceder
-				</button>
-			</form>
+			
 			
 		</fieldset>
-		
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" type="text/javascript"></script>
 	</div>
 	
 </body>
