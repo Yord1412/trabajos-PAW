76a0486f9f0fd9e26ba335f0793f7b13abcc3b8b
Yord1412 <71461787+Yord1412@users.noreply.github.com>
2022-10-19 14:01:31 -0600

    unidad 5 rutas en laravel
    introduccion de laravel y creacion de rutas

---
+++ unidad_5/proyecto/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('saludo/', function(){
+    return "hola";
+});
+    
+Route::get('saludo/{nombre}', function($name){
+    return "hola ".$name;
+});
+
+Route::get('suma/{num1}/{num2}', function($num1,$num2){
+    return $num1 + $num2;
+})-> where('num1','[0-9]+')-> where('num2','[0-9]+');
+//-> where(['num1'=>'[0-9]+','num2'=>'[0-9]+']);
+
+Route::get('multi/{num1}/{num2}/{num3}', function($num1,$num2,$num3 = 1){
+    return $num1 * $num2 * $num3;
+});
